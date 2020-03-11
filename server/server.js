d65196edd795c4a88b0647bbcea597db6b062693
Alberto Vega <albertovega00@gmail.com>
2020-03-11 16:43:01 -0700

    added axios call to server

--- server/server.js
+++ server/server.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const axios = require('axios');
 
 const app = express();
 
 app.use(morgan('dev'));
 app.use(express.static('dist'));
 app.use(express.static('public'));
+
+app.get('/game/:gamesearch', (req, res) => {
+    axios({
+        url: `https://api.rawg.io/api/games?page_size=5&search=gta%20v`,
+        method: 'get',
+    })
+    .then(response => {
+        res.status(200).send(response.data);
+    })
+});
 
 module.exports = app;
