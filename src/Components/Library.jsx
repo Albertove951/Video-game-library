3c28942382b390de72e605ff21430c25c729e243
Alberto Vega <albertovega00@gmail.com>
2020-03-11 13:18:00 -0700

    added library component

---
+++ src/Components/Library.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+
+export default class Library extends Component {
+
+    render() {
+        return(
+            <div className="librabry-container">
+                <div></div>
+            </div>
+        )
+    }
+}
